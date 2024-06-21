d5dbac68b491b5d529f1e2354dd8a1747fcaf979
Sohail4625 <shaiksohail4625@gmail.com>
2024-06-21 22:48:03 +0530

    Changes

--- liveHost/static/liveView.js
+++ liveHost/static/liveView.js
@@ -9,24 +9,31 @@ inn1Button.addEventListener("click", () => {
   inn1Button.style.border = "3px solid black";
   inn2Button.style.border = "none";
   inn = 1;
-  showInningsContent(inn1Content, inn2Content);
-  buildTableBatting(".inn1-batting-container", innings1DataBatting);
-  buildTableBowling(".inn1-bowling-container", innings1DataBowling);
+  if(inn1Content.style.display=="block"){
+    inn1Button.style.border = "none";
+    inn1Content.style.display = "none";
+  }
+  else {
+    inn2Content.style.display = "none";
+    inn1Content.style.display = "block";
+
+  }
 });
 
 inn2Button.addEventListener("click", () => {
   inn2Button.style.border = "3px solid black";
   inn1Button.style.border = "none";
   inn = 2;
-  showInningsContent(inn2Content, inn1Content);
-  buildTableBatting(".inn2-batting-container", innings2DataBatting);
-  buildTableBowling(".inn2-bowling-container", innings2DataBowling);
+  if(inn2Content.style.display=="block"){
+    inn2Button.style.border = "none";
+    inn2Content.style.display = "none";
+  }
+  else {
+    inn1Content.style.display = "none";
+    inn2Content.style.display = "block";
+  }
 });
 
-function showInningsContent(contentToShow, contentToHide) {
-  contentToShow.style.display = "block";
-  contentToHide.style.display = "none";
-}
 var innings1_score = "",
   innings2_score = "";
 function buildTableBatting(containerSelector, playerData) {
@@ -141,11 +148,59 @@ chatSocket.onmessage = function (event) {
     document.getElementById("target").textContent = "Target: " + "-";
   else document.getElementById("target").textContent = "Target: " + data.target;
   document.getElementById("ball1").textContent = data.overs_timeline[0];
+  if(data.overs_timeline[0]=='W'){
+    document.getElementById("ball1").style.backgroundColor = "red";
+    document.getElementById("ball1").style.color = "white";
+  }
+  else if(data.overs_timeline[0]=='4'||data.overs_timeline[0]=='6'){
+    document.getElementById("ball1").style.backgroundColor = "green";
+    document.getElementById("ball1").style.color = "white";
+  }
   document.getElementById("ball2").textContent = data.overs_timeline[1];
+  if(data.overs_timeline[1]=='W'){
+    document.getElementById("ball2").style.backgroundColor = "red";
+    document.getElementById("ball2").style.color = "white";
+  }
+  else if(data.overs_timeline[1]=='4'||data.overs_timeline[1]=='6'){
+    document.getElementById("ball2").style.backgroundColor = "green";
+    document.getElementById("ball2").style.color = "white";
+  }
   document.getElementById("ball3").textContent = data.overs_timeline[2];
+  if(data.overs_timeline[2]=='W'){
+    document.getElementById("ball3").style.backgroundColor = "red";
+    document.getElementById("ball3").style.color = "white";
+  }
+  else if(data.overs_timeline[2]=='4'||data.overs_timeline[2]=='6'){
+    document.getElementById("ball3").style.backgroundColor = "green";
+    document.getElementById("ball3").style.color = "white";
+  }
   document.getElementById("ball4").textContent = data.overs_timeline[3];
+  if(data.overs_timeline[3]=='W'){
+    document.getElementById("ball4").style.backgroundColor = "red";
+    document.getElementById("ball4").style.color = "white";
+  }
+  else if(data.overs_timeline[3]=='4'||data.overs_timeline[3]=='6'){
+    document.getElementById("ball4").style.backgroundColor = "green";
+    document.getElementById("ball4").style.color = "white";
+  }
   document.getElementById("ball5").textContent = data.overs_timeline[4];
+  if(data.overs_timeline[4]=='W'){
+    document.getElementById("ball5").style.backgroundColor = "red";
+    document.getElementById("ball5").style.color = "white";
+  }
+  else if(data.overs_timeline[4]=='4'||data.overs_timeline[4]=='6'){
+    document.getElementById("ball5").style.backgroundColor = "green";
+    document.getElementById("ball5").style.color = "white";
+  }
   document.getElementById("ball6").textContent = data.overs_timeline[5];
+  if(data.overs_timeline[5]=='W'){
+    document.getElementById("ball6").style.backgroundColor = "red";
+    document.getElementById("ball6").style.color = "white";
+  }
+  else if(data.overs_timeline[5]=='4'||data.overs_timeline[5]=='6'){
+    document.getElementById("ball6").style.backgroundColor = "green";
+    document.getElementById("ball6").style.color = "white";
+  }
   if (data.team1 == data.first_batting) {
     innings1DataBatting = data.team1_players;
     innings2DataBowling = data.team1_players;
@@ -167,4 +222,16 @@ chatSocket.onmessage = function (event) {
     data.toss_winner + " won the toss and chose to " + x + " first !";
   innings1_score = data.innings1_score;
   innings2_score = data.innings2_score;
+  if(data.innings_no==1){
+    document.getElementById("curr-run-rate").textContent = parseFloat(data.innings1_score_num/parseFloat(data.overs/6)).toFixed(2);
+    
+  }
+  else {
+    document.getElementById("curr-run-rate").textContent = parseFloat(data.innings2_score_num/parseFloat(data.overs/6)).toFixed(2);
+    document.getElementById("req-run-rate").textContent = parseFloat((data.target - data.innings2_score_num)/parseFloat(((data.maximum_overs)*6 - data.overs)/6)).toFixed(2);
+  }
+  buildTableBatting(".inn1-batting-container", innings1DataBatting);
+  buildTableBowling(".inn1-bowling-container", innings1DataBowling);
+  buildTableBatting(".inn2-batting-container", innings2DataBatting);
+  buildTableBowling(".inn2-bowling-container", innings2DataBowling);
 };
