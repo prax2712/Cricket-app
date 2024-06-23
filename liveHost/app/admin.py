abae869a214b2545711a13cea8e18c53cb88bbdd
prax2712 <118594693+prax2712@users.noreply.github.com>
2024-06-23 17:26:12 +0530

    Update admin.py

--- liveHost/app/admin.py
+++ liveHost/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import player_stats, match_info, innings, player_match_stats,innings,overs_timeline  # Import all your models
 
-# Register your models here.
+
+admin.site.register(match_info)
+admin.site.register(innings)
+admin.site.register(player_match_stats)
+admin.site.register(player_stats)
+admin.site.register(overs_timeline)
