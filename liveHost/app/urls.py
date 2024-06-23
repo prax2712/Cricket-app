0641999af8df5ffbb4aa9bb8bdc5ee24e2d4b8a2
prax2712 <118594693+prax2712@users.noreply.github.com>
2024-06-23 17:08:11 +0530

    Update urls.py

--- liveHost/app/urls.py
+++ liveHost/app/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('liveHost/<str:matchId>/',views.live_host),
-    path('liveView/<str:match_id>/',views.liveView,name='liveView'),
-    path('statistics/',views.statistics,name='statistics'),
-    path('player_selection/<str:match_id>/',views.player_selection,name='player_selection'),
-    path('<str:username>/',views.homepage,name='home'),
     path('',views.homepageNonUser,name='homepageNonUser'),
-    path('toss/<str:match_id>/', views.toss, name='toss'),
+    path('statistics/',views.statistics,name='statistics'),
     path('login/', views.login, name='login'),
     path('registration/', views.signup, name='signup'),
+    path('liveHost/<str:matchId>/',views.live_host),
+    path('liveView/<str:match_id>/',views.liveView,name='liveView'),
+    path('player_selection/<str:match_id>/',views.player_selection,name='player_selection'),
+    path('<str:username>/',views.homepage,name='home'),
+    path('toss/<str:match_id>/', views.toss, name='toss'),
     path('hosting/<str:username>/', views.hosting, name='hosting'),
     path('match_summary/<int:match_id>/',views.match_summary,name='match_summary'),
 ]
